a919ab733103c3ad03ec8921798573f1a9e9a623
FezHacker <fezdcb@mgail.com>
2015-11-07 17:04:19 -0800

    Controlls

--- Conquest/GameScene.swift
+++ Conquest/GameScene.swift
@@ -10,21 +10,24 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    var player: SKSpriteNode!
+    var player_two: SKSpriteNode!
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 45;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        player = SKSpriteNode(color: UIColor.greenColor(), size: CGSize(width: 100, height: 100))
+        player_two = SKSpriteNode(color: UIColor.redColor(), size: CGSize(width: 100, height: 100))
         
         // add player to screen
+        player.position = CGPoint(x: player.size.width / 2, y: size.height / 2)
+        player_two.position = CGPoint(x: size.width - player_two.size.width / 2, y: size.height / 2)
         
-        
-        self.addChild(myLabel)
+        self.addChild(player)
+        self.addChild(player_two)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
+        /* Called when a touch begins */
         
         for touch in touches {
             let location = touch.locationInNode(self)
@@ -42,8 +45,37 @@ class GameScene: SKScene {
             self.addChild(sprite)
         }
     }
-   
+    
+    func moveLeft() {
+        if player.position.x > player.size.width / 2 {
+            player.position.x -= 0.5
+        } else {
+            print("not move")
+        }
+    }
+    
+    func moveRight() {
+        if player.position.x < size.width - player.size.width / 2 {
+            player.position.x += 0.5
+        } else {
+            print("not move")
+        }
+    }
+    
+    func distance() -> CGFloat {
+        let xDist = CGFloat(player_two.position.x - player.position.x)
+        let yDist = CGFloat(player_two.position.y - player.position.y)
+        let distance = sqrt((xDist * xDist) + (yDist * yDist))
+        
+        return distance
+    }
+    
+    func attack() {
+        
+    }
+    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        moveRight()
     }
 }
