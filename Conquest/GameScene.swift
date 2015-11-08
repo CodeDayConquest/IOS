79413455df0b830259a5fc7f6c509ce8e95b69b6
FezHacker <fezdcb@mgail.com>
2015-11-07 19:54:04 -0800

    some shit

--- Conquest/GameScene.swift
+++ Conquest/GameScene.swift
@@ -12,11 +12,15 @@ class GameScene: SKScene {
     
     var player: Player! = nil
     var player_two: Player! = nil
+    
     var counter: Int = 0
+    
     var rightButton: SKSpriteNode! = nil
     var leftButton: SKSpriteNode! = nil
+    var jumpButton: SKSpriteNode! = nil
+    var punchButton: SKSpriteNode! = nil
+    var kickButton: SKSpriteNode! = nil
     var blockButton: SKSpriteNode! = nil
-    var attackButton: SKSpriteNode! = nil
     
     override func didMoveToView(view: SKView)
     {
@@ -29,19 +33,32 @@ class GameScene: SKScene {
         /// Player Two
         player_two = Player(health: 100, size: CGSize(width: 100, height: 100), pos: CGPoint(x: size.width * 0.75, y: size.height / 2.0), playerImage: nil, level: 1, playerXP: 100);
         
-        // add Buttons
-        rightButton = SKSpriteNode(color: UIColor.redColor(), size: CGSize(width: size.width / 4, height: size.height / 3))
-        rightButton.position = CGPoint(x: rightButton.size.width / 2, y: rightButton.size.height / 2)
+        // Add Buttons to Screen
         
-        leftButton = SKSpriteNode(color: UIColor.redColor(), size: CGSize(width: size.width / 4, height: size.height / 3))
-        leftButton.position = CGPoint(x: size.width / 2 + leftButton.size.width / 2, y: leftButton.size.height / 2)
+        /// Right Button
+        //rightButton = SKSpriteNode(color: UIColor.redColor(), size: CGSize(width: size.width / 4, height: size.height / 3))
+        //rightButton.position = CGPoint(x: rightButton.size.width / 2, y: rightButton.size.height / 2)
         
+        /// Left Button
+        //leftButton = SKSpriteNode(color: UIColor.redColor(), size: CGSize(width: size.width / 4, height: size.height / 3))
+        //leftButton.position = CGPoint(x: size.width / 2 + leftButton.size.width / 2, y: leftButton.size.height / 2)
+        
+        /// Jump Button
+        
+        /// Punch Button
+        punchButton = SKSpriteNode(color: UIColor.purpleColor(), size: CGSize(width: size.width / 4, height: size.height / 3))
+        punchButton.position = CGPoint(x: punchButton.size.width / 2, y: punchButton.size.height / 2)
+        
+        /// Kick Button
+        
+        /// Block Button
 
         // Add objects to Screen
         self.addChild(player)
         self.addChild(player_two)
-        self.addChild(rightButton)
-        self.addChild(leftButton)
+        //self.addChild(rightButton)
+        //self.addChild(leftButton)
+        self.addChild(punchButton)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -53,9 +70,14 @@ class GameScene: SKScene {
                 moveRight()
                 print("right!")
             }
+            
             if leftButton.containsPoint(location) {
                 moveLeft()
                 print("left")
+            }
+            
+            if punchButton.containsPoint(location) {
+                attack(10, block: true)
             }
             
         }
@@ -85,8 +107,7 @@ class GameScene: SKScene {
             xDist = CGFloat(player_two.position.x - player_two.size.width / 2) - CGFloat(player.position.x + player.size.width / 2)
         } else if player.position.x - player.size.width / 2 >  player_two.position.x + player_two.size.width / 2 {
             xDist = CGFloat(player.position.x - player.size.width / 2) - CGFloat(player_two.position.x + player_two.size.width / 2)
-        }
-        else {
+        } else {
             xDist = 0;
         }
         
@@ -94,9 +115,7 @@ class GameScene: SKScene {
             yDist = CGFloat(player_two.position.y - player_two.size.height / 2) - CGFloat(player.position.y + player.size.height / 2)
         } else if player.position.y - player.size.height / 2 > player_two.position.y + player_two.size.width / 2 {
             yDist = (player.position.y - player.size.height / 2) - (player_two.position.y + player_two.size.height / 2)
-        }
-        
-        else {
+        } else {
             yDist = 0
         }
         
@@ -107,14 +126,19 @@ class GameScene: SKScene {
         return distance
     }
     
-    func attack() {
-        
-        if distance() < player.size.width * 2
-        {
+    func attack(damage: Int, block: Bool) {
+        if distance() < player.size.width {
+            let randDamage = Float(arc4random_uniform(UInt32(damage)))
+            var damageReduction = Float()
             
+            if block {
+                damageReduction = Float(arc4random_uniform(UInt32(100)))
+            } else {
+                print("No block")
+            }
+            
+            player.playerHealth -= randDamage - damageReduction
         }
-        
-        
     }
     
     func randomNumber() -> Int {
@@ -123,13 +147,6 @@ class GameScene: SKScene {
     }
     
     override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
-//        if counter < 200
-//        {
-//            moveRight()
-//        } else{
-//            moveLeft()
-//        }
         print(distance())
         counter++
     }
