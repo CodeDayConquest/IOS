ed92e66286e7b2723c66329ce4cfd124e461cb8e
Zafir Abou-Zamzam <z.abouzamzam@gmail.com>
2015-11-07 17:02:22 -0800

    Updated player class

--- Conquest/Player.swift
+++ Conquest/Player.swift
@@ -6,8 +6,35 @@
 //  Copyright © 2015 Alpha. All rights reserved.
 //
 
-import Cocoa
+import SpriteKit
 
-class Player: SKNode {
+class Player: SKSpriteNode
+{
+    
+    var playerHealth: Float
+    var playerSize: CGSize
+    var playerPos: CGPoint
+    var playerImage: SKTexture?
+
+    init(health: Float, size: CGSize, pos: CGPoint, playerImage: SKTexture?)
+    {
+        
+        self.playerHealth = health
+        self.playerSize = size
+        self.playerPos = pos
+        self.playerImage = playerImage
+        
+        
+        super.init(texture: playerImage, color: UIColor.greenColor(), size: size)
+
+        self.position = self.playerPos
+
+        
+
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
 }
